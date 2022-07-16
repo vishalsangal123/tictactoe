08c629f65afe5180b3573ccbfb7f4926d5f18e56
Vishal Sangal <vishalsangal40@gmail.com>
2022-07-16 14:23:34 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,56 @@
-import React,{useState} from 'react';
-import Board from './component/Board';
-import History from './component/History';
-import statusMessage from './component/statusMessage';
-import './styles/root.scss';
-import { calculateWinner } from '../helper';
-// export default () => (
-//   <>
-//     <h1>Welcome to React Vite Micro App!</h1>
-//     <p>Hard to get more minimal than this React app.</p>
-//   </>
-// );
-// const NEW_GAME=[
-//   {board: Array(9).fill(null),isXNext: true}
-// ]
+import React, { useState } from 'react';
+import Board from './components/Board';
+import History from './components/History';
+import { calculateWinner } from '/helper';
 
+import './styles/root.scss';
 
 const App = () => {
-  const [history,setHistory]=useState([
-    {board: Array(9).fill(null),isXNext: true}
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
   ]);
-// console.log(board);
-// console.log('board rerender');
-const [currentMove,setCurrentMove]=useState(0);
-const current=history[currentMove];
- //const {winner,winningSquares}=calculateWinner(current.board);
-// // console.log(winner);
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove];
 
- const message=winner?`Winner is ${winner}`:`Next player is ${current.isXNext?'X':'O'}`
+  const winner = calculateWinner(current.board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
-
-const handleSquareClick=(position)=>{
-
-  if(current.board[position] || winner )
-  return;
-  setHistory((prev)=>{
-    const last=prev[prev.length-1];
-    const newBoard= last.board.map((square,pos)=>
-    {
-      if(pos===position)
-    {
-      return last.isXNext? 'X':'O';
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
+      return;
     }
-    return square;
-    })
-    return prev.concat({board:newBoard,isXNext:!last.isXNext});
-  });
-  setCurrentMove(prev=>prev+1);
-};
-// const onNewGame=()=>{
-// setHistory(NEW_GAME);
-// setCurrentMove(0);
-// };
 
-const moveTo = (move)=>{
-  setCurrentMove(move);
-}
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
   return (
     <div className="app">
-      <h1> TIC TAC TOE</h1> 
-    <statusMessage winner={winner} current={current}/>
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
-      {/* <button type="button" onclick={onNewGame}>Start New Game</button> */}
-      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
+
 export default App;
